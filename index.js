8b5fcd6b0acf9c19274df1d0bd3a4a70d171c51b
LucioFex <luciano1D2esteban@gmail.com>
2021-07-10 16:49:38 -0300

    feat: Implementation of the addListener in the JS file

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 /* JS document */
+
+const setUp = () => {
+    const bossLore = document.getElementsByClassName("boss-lore");
+    const cubeBossOne = document.getElementsByClassName("face");
+    const bossIcons = document.getElementsByClassName("boss-icon");
+    const gameChanger = document.getElementById("change-game");
+}
+
+window.addEventListener('load', setUp, false);  // Starts the script
