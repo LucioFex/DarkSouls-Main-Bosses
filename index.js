1c51724df54dfa8b765053dae2d75939ea811d0c
LucioFex <luciano1D2esteban@gmail.com>
2021-07-11 09:58:46 -0300

    feat: Addition of a bottom margin of the center-div

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const setUp = () => {
     const cubeBossOne = document.getElementsByClassName("face");
     const bossIcons = document.getElementsByClassName("boss-icon");
     const gameChanger = document.getElementById("change-game");
+    const cube3D = document.getElementById("cube");
 }
 
-window.addEventListener('load', setUp, false);  // Starts the script
+const cubeAnimations = () => {
+    cube3D.style.transition = "all 2s";
+    cube3D.style.transform = "rotateY(90deg)";
+}
+
+
+
+window.addEventListener('load', setUp, false); // Starts the script
