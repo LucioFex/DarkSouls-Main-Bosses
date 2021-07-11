d1cd46d7aaa6aaa729e9b8c869dd705a47cd7c45
LucioFex <luciano1D2esteban@gmail.com>
2021-07-11 16:56:26 -0300

    feat: Change of the text info with the boss icons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* JS document */
 
-const bossLore = document.getElementsByClassName("boss-lore");
+const bossInformation = document.getElementsByClassName("boss-lore");
 const cubeBossOne = document.getElementsByClassName("face");
 const bossIcons = document.getElementsByClassName("boss-icon");
 const gameChanger = document.getElementById("change-game");
@@ -17,21 +17,29 @@ const changeBossButton = (index, boss) => {
 }
 
 const getBossData = async () => {
-    const request = "json/dark-souls-1.json";
-    const response = await fetch(request);
-    const json = await response.json();
+    let request = "json/dark-souls-1.json";
+    let response = await fetch(request);
+    let json = await response.json();
     return json
 }
 
 const changeBoss = async (boss) => {
-    const data = await getBossData()
+    let data = await getBossData()
+
     cubeAnimation(data[boss].degrees);
+    bossDescription(data[boss])
 }
 
 const cubeAnimation = (degrees) => {
     cube3D.style.transition = "ease-in-out 2.5s";
     cube3D.style.transform = `rotateY(${degrees})`;
     setTimeout(() => cube3D.style.transition = "all 0s", 250); // No transition
+}
+
+const bossDescription = (description) => {
+    bossInformation[0].innerHTML = description.name;
+    bossInformation[1].innerHTML = description.nickname;
+    bossInformation[2].innerHTML = description.lore;
 }
 
 
